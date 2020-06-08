16746e0639b0e86c10fff246fe8301d629feb56d
oskojess <jessica_osko@hotmail.com>
2020-06-08 13:34:57 -0300

    adicionar exercicio de funcoes

--- aula-01/funcoes/exercicio_04.js
+++
@@ -1,3 +0,0 @@
-/*Escreva uma função que aceita um parâmetro e retorna um tipo.
-Obs.: Existem seis tipos de valores possíveis que typeof retorna: object, boolean,
-function, number, string e undefined. */
