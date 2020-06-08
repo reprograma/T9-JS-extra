a203a4b47dd02c531382bd88d6f21d4792f24023
Vanessa Mello <nessa_comex@hotmail.com>
2020-06-08 16:07:17 -0300

    file-second

--- aula-01/funcoes/exercicio_01.js
+++ aula-01/funcoes/exercicio_01.js
@@ -8,8 +8,15 @@ const endereco = {
 };
 
 /*Retorne o seguinte conteúdo:
+// A função é um bloco de executações, pois assim que você chamar um bloco ela vai ter algo para executar
 
 A {nome} {Sobrenome} mora em São Paulo / SP, no bairro Centro, na rua "Rua dos Pinheiros" com
 nº 1293.
 
 O retorno deverá ser template string*/
+function pegarNomeCompleto(nome, sobrenome){
+  const modificarNome = nome.toUpperCase();//Caso eu queira que fique el letra maíuscula
+  return `${modificarNome} ${sobrenome}`
+}
+ 
+console.log(pegarNomeCompleto("Vanessa", "Mello"))
