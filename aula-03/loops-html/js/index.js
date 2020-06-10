3e2bc54b6872d7fdafbdb94c7179764dd8d2f8fc
oskojess <jessica_osko@hotmail.com>
2020-06-10 15:48:39 -0300

    exercicios de arrow functions

---
+++ aula-03/loops-html/js/index.js
@@ -0,0 +1,20 @@
+const alunas = ["Od", "Nayla", "Sindi",
+  "Maria", "Liana", "Viviane",
+  "Lorena", "Nathalia",
+  "Raquel", "Raquel", "Marisa",
+  "Nathy", "Patricia",
+  "Jordana", "Ilza",
+  "Talisia", "Débora", "Tabita", "Gabrielli",
+  "Claudia", "Sara", "Jemima", "Suzelen", "Giulia", "Vanessa",
+  "Carol", "Ariane", "Millena", "Adela",
+  "Gabi Fernandes"];
+
+const lista = document.getElementById("lista-alunas");
+
+alunas.forEach((aluna) => {
+  let item = document.createElement("li");
+  item.textContent = aluna;
+
+  lista.appendChild(item)
+})
+
